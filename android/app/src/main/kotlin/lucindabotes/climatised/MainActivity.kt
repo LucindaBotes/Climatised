8df4ddcbbac28bb0b1c57e2a8d488edd140d3c1f
LucindaBotes <lucinda.b2000@gmail.com>
2020-08-20 12:23:06 +0200

    Add geolocator and OpenWeatherMap API

---
+++ android/app/src/main/kotlin/lucindabotes/climatised/MainActivity.kt
@@ -0,0 +1,6 @@
+package lucindabotes.climatised
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:climatised/services/location.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,17 +9,39 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+
+
+  void getLocation() async{
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.latitude);
+    print(location.longtitude);
+  }
+
+  void getData() async {
+    Response response = await get('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=439d4b804bc8187953eb36d2a8c26a02');
+     print(response.body);
+
+     if (response.statusCode == 200) {
+       String data = response.body;
+       print(data);
+
+       var lontitude = jsonDecode(data)['coord']['lon'];
+       print(lontitude);
+     } else {
+       print(response.statusCode);
+     }
+  }
+
+  @override
+  void initState() {
+    getLocation();
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
-        ),
-      ),
-    );
+    return Container();
   }
 }
